498ab3e6bf26089a6834cf3d9a7fee66819ea2b1
Kenjiro56 <hiroken506@gmail.com>
2024-08-20 00:04:37 +0900

    feat/gif

--- src/Fortune.tsx
+++ src/Fortune.tsx
@@ -3,25 +3,31 @@ import React, { useState } from "react";
 import { fortunes, Fortune as FortuneType } from "./data";
 import './App.css';
 
+
 const Fortune: React.FC = () => {
     const [fortune, setFortune] = useState<FortuneType | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
     
     const drawFortune = () => {
+        setIsLoading(true);
+        setTimeout(() => {
         const randomValue = Math.random();
 
-    let selectedFortune: FortuneType;
-    if (randomValue < 0.1) {
-        // Sランクの中からランダムに選ぶ
-        selectedFortune = fortunes.filter(f => f.rank === "S")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "S").length)];
-    } else if (randomValue < 0.3) {
-        // Aランクの中からランダムに選ぶ
-        selectedFortune = fortunes.filter(f => f.rank === "A")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "A").length)];
-    } else {
-        // Bランクの中からランダムに選ぶ
-        selectedFortune = fortunes.filter(f => f.rank === "B")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "B").length)];
-    }
+        let selectedFortune: FortuneType;
+        if (randomValue < 0.1) {
+            // Sランクの中からランダムに選ぶ
+            selectedFortune = fortunes.filter(f => f.rank === "S")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "S").length)];
+        } else if (randomValue < 0.3) {
+            // Aランクの中からランダムに選ぶ
+            selectedFortune = fortunes.filter(f => f.rank === "A")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "A").length)];
+        } else {
+            // Bランクの中からランダムに選ぶ
+            selectedFortune = fortunes.filter(f => f.rank === "B")[Math.floor(Math.random() * fortunes.filter(f => f.rank === "B").length)];
+        }
 
-    setFortune(selectedFortune);
+        setFortune(selectedFortune);
+        setIsLoading(false);
+     }, 2000);
     };
     
     const resetFortune = () => {
@@ -31,8 +37,11 @@ const Fortune: React.FC = () => {
     return (
         <div style={{ textAlign: "center", padding: "20px" }}>
         <h1>メンターガチャ</h1>
-        <button onClick={drawFortune}>ガチャを引く</button>
-        {fortune && (
+        {!isLoading &&  <button onClick={drawFortune}>ガチャを引く</button>}
+        {isLoading ? (
+            <img src="./images/dummy.gif" alt="loading" style={{ width: "200px", marginTop: "20px" }} />
+        ) : (
+            fortune && (
                     <div style={{ marginTop: "20px" }}>
                         {/* <img src={fortune.image} alt={fortune.name} style={{ width: "200px" }} /> */}
                         <p>src={fortune.name}</p>
@@ -41,8 +50,8 @@ const Fortune: React.FC = () => {
                             リセット
                         </button>
                     </div>
-
-        )}
+                )
+            )}
         </div>
     );
 };

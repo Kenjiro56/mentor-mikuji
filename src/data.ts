0b311c27b958cd58dc0df4bf2f82a1f13939cbf9
Kenjiro56 <hiroken506@gmail.com>
2024-08-19 21:01:35 +0900

    fix/data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,80 +1,50 @@
 // src/data.ts
 
 export interface Fortune {
-    name: string;
-    message: string;
     image: string;
 }
 
 export const fortunes: Fortune[] = [
     {
-        name: "じろけん",
-        message: "今日は素晴らしい一日になるでしょう！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "かわすけ",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "さりゅ",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "あやち",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "もつ",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "ふくら",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "モーモン",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "てんとー",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "あるちゃん",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "じーふ",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "ちーみょん",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "にとりん",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "スイッチロー",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     {
-        name: "えぐっち",
-        message: "おじさんっていうな！",
         image: "/images/jiroken.jpg"
     },
     ];
